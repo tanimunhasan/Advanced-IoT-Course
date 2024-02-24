09a36aa2844507b7b32435220629655f47b03184
Tanimun Hasan <59294968+tanimunhasan@users.noreply.github.com>
2024-02-24 23:35:23 +0000

    Add files via upload

---
+++ Web/dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+/****************************************************/
+/************** Prepared by : Tanimun Hasan ************/
+/***************     technotraroboticsbd@gmail.com *************/
+/***************     www.technotrarobotics.com *************/
+/****************************************************/ 
+
+define('DB_USER', "id17361767_test");     // Your database user name
+define('DB_PASSWORD', "WI{!8]8JziT[g&S<);			// Your database password (mention your db password here)
+define('DB_DATABASE', "id17361767_testpurpose"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+
+?>
